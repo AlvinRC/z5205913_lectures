87c252792600247b502f06318769c53d817a4caa
Alvin Cheng <z5205193@ad.unsw.edu.au>
2023-04-13 20:52:11 +1000

    first commit

---
+++ toolkit_config.py
@@ -0,0 +1,12 @@
+""" toolkit_config.py
+Project configuration file
+"""
+import os
+# PRJDIR = '/home/fintec/pycharm/toolkit'
+
+#or if Windows:
+# Using double backslashes
+PRJDIR = 'C:\\Users\\Alvin\\PycharmProjects\\toolkit'
+# # Using raw strings
+# # PRJDIR = r'C:\Users\some\other\directory'
+DATADIR = os.path.join(PRJDIR, 'data')
